0639eacb15a29470cf8edbf69db60d5a883b8d8d
Jared King <j@jaredtking.com>
2014-08-18 22:09:32 -0500

    improved tests further

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,7 +6,7 @@ define( 'INFUSE_BASE_DIR', __DIR__ );
 set_include_path( get_include_path() . PATH_SEPARATOR . INFUSE_BASE_DIR );
 
 // shim for users model
-require_once 'src/app/auth/DefaultUser.php';
+require_once 'UserShim.php';
 
 // This is a hack for the tests to generate a table for the users
 \app\auth\Controller::$properties[ 'models' ][] = 'User';
