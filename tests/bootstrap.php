5dafc98c7efd903c8d84cf0332e9b9f229f1ed43
Jared King <j@jaredtking.com>
2014-08-18 21:09:56 -0500

    moved DB schema installation into tests/bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,4 +1,15 @@
 <?php
 
+require_once 'vendor/autoload.php';
+
 define( 'INFUSE_BASE_DIR', __DIR__ );
 set_include_path( get_include_path() . PATH_SEPARATOR . INFUSE_BASE_DIR );
+
+/* Install DB Schema */
+
+$config = @include 'config.php';
+$config[ 'modules' ][ 'middleware' ] = [];
+$config[ 'sessions' ][ 'enabled' ] = false;
+$app = new App( $config );
+
+$app->installSchema( true );
