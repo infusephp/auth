9c057321df021a27d0ebab7cb212aab82072da45
Jared King <j@jaredtking.com>
2014-08-23 17:10:02 -0500

    changed userlinks migration

--- src/app/auth/migrations/20140823144638_user_link.php
+++ src/app/auth/migrations/20140823144638_user_link.php
@@ -14,7 +14,7 @@ class UserLink extends AbstractMigration
             $table = $this->table( 'UserLinks', [ 'id' => false, 'primary_key' => [ 'uid', 'link' ] ] );
             $table->addColumn( 'uid', 'integer' )
                   ->addColumn( 'link', 'string', [ 'length' => 32 ] )
-                  ->addColumn( 'link_type', 'integer', [ 'length' => 2 ] )
+                  ->addColumn( 'link_type', 'integer' )
                   ->addColumn( 'created_at', 'integer' )
                   ->addColumn( 'updated_at', 'integer', [ 'null' => true, 'default' => null ] )
                   ->create();
