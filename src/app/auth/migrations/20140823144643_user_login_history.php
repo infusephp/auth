9e5909e213427b3c83fd638bca62c41c76ece77d
Jared King <j@jaredtking.com>
2014-08-27 18:47:08 -0500

    bump up user login history ip character length for ipv6

--- src/app/auth/migrations/20140823144643_user_login_history.php
+++ src/app/auth/migrations/20140823144643_user_login_history.php
@@ -14,7 +14,7 @@ class UserLoginHistory extends AbstractMigration
             $table = $this->table( 'UserLoginHistories' );
             $table->addColumn( 'uid', 'integer' )
                   ->addColumn( 'type', 'integer', [ 'length' => 1 ] )
-                  ->addColumn( 'ip', 'string', [ 'length' => 16 ] )
+                  ->addColumn( 'ip', 'string', [ 'length' => 45 ] )
                   ->addColumn( 'user_agent', 'string' )
                   ->addColumn( 'created_at', 'integer' )
                   ->addColumn( 'updated_at', 'integer', [ 'null' => true, 'default' => null ] )
